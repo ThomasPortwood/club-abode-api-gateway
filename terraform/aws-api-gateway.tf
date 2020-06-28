b3f100db601c155334f2193342290ae838e8f573
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 09:13:53 -0400

    update route selection

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_apigatewayv2_api" "club-abode-api-gateway" {
   name                       = "club-abode-api-gateway"
   protocol_type              = "HTTP"
-  route_selection_expression = "$request.body.action"
+  route_selection_expression = "$request.method"
 }
