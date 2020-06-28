9b5949d5945f466a5a321875c3d07ce1d803c4bd
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 08:44:06 -0400

    add authorizer

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -9,6 +9,11 @@ resource "aws_api_gateway_resource" "club-abode-resource" {
   path_part   = "rest"
 }
 
+resource "aws_api_gateway_authorizer" "club-abode-auth0-authorizer" {
+  name                   = "club-abode-auth0-authorizer"
+  rest_api_id            = aws_api_gateway_rest_api.club-abode-gateway.id
+}
+
 resource "aws_api_gateway_method" "club-abode-method" {
   rest_api_id   = aws_api_gateway_rest_api.club-abode-gateway.id
   resource_id   = aws_api_gateway_resource.club-abode-resource.id
