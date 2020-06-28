20f1faab0581d1d8a4a81428d1c44387a2695e1f
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 08:26:51 -0400

    add gitignore, update gateway name

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "club-abode-gateway" {
-  name        = "Club-Abode"
+  name        = "club-abode-gateway"
   description = "This is my API for demonstration purposes"
 }
 
