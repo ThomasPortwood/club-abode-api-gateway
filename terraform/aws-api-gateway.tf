80cfae6d8fe1381f25ee0c0aa7e4d64c8597bfcd
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 12:34:23 -0400

    add integration uri

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -19,6 +19,7 @@ resource "aws_apigatewayv2_integration" "club-abode-integration" {
   api_id             = aws_apigatewayv2_api.club-abode-api.id
   integration_type   = "HTTP_PROXY"
   integration_method = "ANY"
+  integration_uri = "https://e35e00df-graphql-graphql-41d3-2003767172.us-east-1.elb.amazonaws.com"
 }
 
 resource "aws_apigatewayv2_route" "club-abode-route" {
