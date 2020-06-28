99da389009d8bf57cbf09e7f57a20fef3202e272
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 08:32:32 -0400

    create resource method

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -8,3 +8,10 @@ resource "aws_api_gateway_resource" "club-abode-resource" {
   parent_id   = aws_api_gateway_rest_api.club-abode-gateway.root_resource_id
   path_part   = "rest"
 }
+
+resource "aws_api_gateway_method" "club-abode-method" {
+  rest_api_id   = aws_api_gateway_rest_api.club-abode-gateway.id
+  resource_id   = aws_api_gateway_resource.club-abode-resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
