da7cab3ef648eb5267ca40bcdbc27597ca0d3525
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 13:30:33 -0400

    fix methods

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -18,13 +18,14 @@ resource "aws_apigatewayv2_authorizer" "club-abode-auth0-authorizer" {
 resource "aws_apigatewayv2_route" "club-abode-route" {
   api_id        = aws_apigatewayv2_api.club-abode-api.id
   route_key     = "/graphql"
+  operation_name = "POST"
   authorizer_id = aws_apigatewayv2_authorizer.club-abode-auth0-authorizer.id
 }
 
 resource "aws_apigatewayv2_integration" "club-abode-integration" {
   api_id             = aws_apigatewayv2_api.club-abode-api.id
   integration_type   = "HTTP_PROXY"
-  integration_method = "ANY"
+  integration_method = "POST"
   integration_uri = "https://e35e00df-graphql-graphql-41d3-2003767172.us-east-1.elb.amazonaws.com"
 }
 
