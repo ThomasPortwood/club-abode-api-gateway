faa76d8520e6e0dbc75c21914ab4a567be5ab74a
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-28 09:07:55 -0400

    switch to v2 gateway

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -1,22 +1,5 @@
-resource "aws_api_gateway_rest_api" "club-abode-gateway" {
-  name        = "club-abode-gateway"
-  description = "This is my API for demonstration purposes"
-}
-
-resource "aws_api_gateway_resource" "club-abode-resource" {
-  rest_api_id = aws_api_gateway_rest_api.club-abode-gateway.id
-  parent_id   = aws_api_gateway_rest_api.club-abode-gateway.root_resource_id
-  path_part   = "rest"
-}
-
-resource "aws_api_gateway_authorizer" "club-abode-auth0-authorizer" {
-  name                   = "club-abode-auth0-authorizer"
-  rest_api_id            = aws_api_gateway_rest_api.club-abode-gateway.id
-}
-
-resource "aws_api_gateway_method" "club-abode-method" {
-  rest_api_id   = aws_api_gateway_rest_api.club-abode-gateway.id
-  resource_id   = aws_api_gateway_resource.club-abode-resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
+resource "aws_apigatewayv2_api" "club-abode-api-gateway" {
+  name                       = "club-abode-api-gateway"
+  protocol_type              = "HTTP"
+  route_selection_expression = "$request.body.action"
 }
